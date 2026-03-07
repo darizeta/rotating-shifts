e7fe01091348780b11c7c2ff96bb37baebadf5a1
Ivan Zambrano <darizeta@hotmail.com>
2026-03-07 18:54:49 -0500

    Se actualiza el turno

--- js/turno_asignado.js
+++ js/turno_asignado.js
@@ -1,11 +1,9 @@
 const turnoAsignadoDiv = document.getElementById('turno_asignado');
-const nombreSpan = document.getElementById('nombre');
-
 const nombreUsuario = sessionStorage.getItem('nombreUsuario') || 'Usuario';
 const html=
-`<div style="color: #6A52CB; text-align: center; font-size: 2rem; display: flex; flex-direction: column; align-items: center;"><h2>Hola : <span id="nombre">${nombreUsuario}</span></h2>
-    <span><h2 style="margin-top:-2rem; font-size:2.5rem; color:#733F3F">Tu turno es:</h2></span>
-    <div id="turn" style="border-radius:50%; border:1px solid red; width: 250px; height: 250px;"></div>
+`<div style="color: #010006; text-align: center; font-size: 3rem; display: flex; flex-direction: column; align-items: center;"><h2> <span id="nombre">${nombreUsuario}</span></h2>
+    <span><h3 style="margin-top:-2rem; font-size:2.5rem; color:#733F3F">Tu turno es:</h3></span>
+    <div id="turn" style="border-radius:50%; margin-top:-30px; border:2px solid red; width: 300px; height: 300px; background-color: #f5eeee"></div>
     <button id="aceptarTurno" type="submit" style="margin-top: 20px; padding: 15px 50px; background-color: #3813cdff; color: white; border: none; border-radius: 5px; cursor: pointer; font-size: 1rem;
     transition: background-color 0.3s;">Iniciar</button>
 </div>
@@ -13,6 +11,9 @@ const html=
 
 // Insertar el HTML en el div turno_asignado
 turnoAsignadoDiv.innerHTML = html;
+const h3 = document.querySelector('h3');
+h3.style.fontFamily = 'monospace';
+h3.style.color = '#210d6d';
 
 // Agregar evento para cambiar el color del botón al pasar el mouse
 const botonTurnos = document.getElementById('aceptarTurno');
@@ -34,20 +35,21 @@ botonTurnos.addEventListener('click', function() {
 function generarTurno() {
     // Deshabilitar el botón durante la animación
     botonTurnos.disabled = true; 
-    turnDiv.style.border='None'
-    turnDiv.style.borderBottom = '4px solid red';
-    turnDiv.style.animation = 'girar 5s linear infinite';
-    turnDiv.style.width = '250px';
-    turnDiv.style.height = '250px';
-    turnDiv.style.borderRadius = '50%';
+    turnDiv.style.border = '2px solid red';
+    turnDiv.style.animation = 'girar 4.5s ease-out';
+    turnDiv.style.borderLeftColor = 'transparent';
+    turnDiv.style.borderRightColor = 'transparent';
+    turnDiv.style.display = 'flex';
+    turnDiv.style.justifyContent = 'center';
+    turnDiv.style.alignItems = 'center';
+    turnDiv.style.width = '300px';
+    turnDiv.style.height = '300px';
     const textoTurno = document.getElementById('aceptarTurno');
     textoTurno.textContent = 'Generando turno...';
     const aceptar = 'Aceptar'
     // Detener la animación después de 3 segundos y mostrar el turno asignado
     setTimeout(function() {
-        const puesto=['ADM=F1','VD=F2','APOYO ARCO=F3','CTR.Z.AIRE=F4','ARCO=F5','DESARMADOR=F6','DESEMBARQUE=F7','VOR=F8','CA.NORTE=F9','CA.SUR=F10',
-        'MA. RAYOS X=F11','DET.METALES=F12','RADAR=F13','ARMADOR=F14','SÓTANO=F15','INGR.AREAS.REST=F16','SENDA.PLANEO=F17','A.SALA=F18','A.ARE.RESTRI=F19','PARQUEADERO=F20',
-        'DISPONIBLE=F21','A.PARQUEADERO=F23','A.C.SUR=24','A.PARQUE=F25','A.NORTE=F26','A.RADAR=F27'];
+        const puesto=['Puesto 1', 'Puesto 2', 'Puesto 3', 'Puesto 4', 'Puesto 5', 'Puesto 6', 'Puesto 7', 'Puesto 8'];
         // Generar un índice aleatorio para seleccionar un turno del arreglo
         const turnoNumero = document.createElement('span');
         if (puesto.length > 0) {
@@ -55,18 +57,19 @@ function generarTurno() {
             const turnoAsignado = puesto[indiceAleatorio];  
             turnoNumero.textContent = turnoAsignado;
             turnoNumero.style.fontSize = '2rem';
-            turnoNumero.style.padding = '10px 20px';
-            turnoNumero.style.marginBottom = '20px';
             turnoNumero.style.borderRadius = '5px';
+            turnoNumero.style.borderBottom = '2px solid #000000';
             turnoNumero.style.backgroundColor = '#ffffff';
             turnoNumero.style.color = 'rgb(228, 14, 121)';
+            turnoNumero.style.padding = '10px 10px';
             turnoNumero.style.fontWeight = 'bold';
             // Detener la animación y mostrar el turno asignado
-            turnDiv.style.animation = 'none';
-            turnDiv.style.border = 'none';
             turnDiv.style.display = 'flex';
             turnDiv.style.justifyContent = 'center';
             turnDiv.style.alignItems = 'center';
+            turnDiv.style.animation = 'none';
+            turnDiv.style.border = '1px solid #f10808';
+            turnDiv.style.backgroundColor = '#f5eeee';
             textoTurno.textContent = `${aceptar}`;
             textoTurno.style.backgroundColor = '#4CAF50'; // Cambiar color a verde
             
